628d348e9d398b96e454a294f624c5e68ec4673c
Jackson Duarte <jacksonwillerduarte@gmail.com>
2025-04-08 20:37:03 -0300

    feat: initialize Angular client application with basic structure and dependencies
    - Added package.json with Angular and PrimeNG dependencies.
    - Created app-routing.module.ts for routing configuration.
    - Implemented app.component.ts and app.component.html for the main application component.
    - Set up app.module.ts to bootstrap the application.
    - Created index.html as the entry point for the application.
    - Added main.ts to bootstrap the Angular application.
    - Included global styles in styles.scss and imported necessary PrimeNG styles.
    - Configured test.ts for Karma testing setup.
    - Defined TypeScript configuration files (tsconfig.app.json, tsconfig.json, tsconfig.spec.json) for the application and tests.

---
+++ Front/cliente-app/src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+
+@NgModule({
+    imports: [
+        
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
---
+++ Front/cliente-app/src/app/app.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { HashLocationStrategy, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
+
+export class AppModule {}
