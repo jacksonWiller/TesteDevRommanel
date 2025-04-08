49ce6a6d3fcdca602cf3acb0adf2f63885fb84a1
Jackson Duarte <jacksonwillerduarte@gmail.com>
2025-04-08 03:09:04 -0300

    adicionando interface IClienteRepository com métodos para manipulação de clientes

---
+++ Back/Cliente.Dominio/Interfaces/IClienteRepository.cs
@@ -0,0 +1,15 @@
+﻿using Clientes.Dominio.Entidades;
+
+namespace Clientes.Dominio.Interfaces
+{
+    public interface IClienteRepository
+    {
+        Task<Cliente[]> GetAllClientesAsync();
+        Task<Cliente[]> GetClientesByNomeAsync(string nome);
+        Task<Cliente> GetClienteByIdAsync(Guid clienteId);
+        Task<Cliente> GetClienteByDocumentoAsync(string documento);
+        Task<Cliente> GetClienteByEmailAsync(string email);
+        Task<bool> ExisteClienteComDocumentoAsync(string documento);
+        Task<bool> ExisteClienteComEmailAsync(string email);
+    }
+}
--- Back/Clientes.Infra/Repositorio/ClienteRepository.cs
+++ Back/Clientes.Infra/Repositorio/ClienteRepository.cs
@@ -1,12 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Clientes.Dominio.Entidades;
+using Clientes.Dominio.Interfaces;
+using Clientes.Infra.Contexto;
+using Microsoft.EntityFrameworkCore;
 
-namespace Clientes.Infra.Repositorio
+namespace Clientes.Infra.Repositorios
 {
-    internal class ClienteRepository
+    public class ClienteRepository : IClienteRepository
     {
+        private readonly ClienteContext _dataContext;
+
+        public ClienteRepository(ClienteContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<Cliente[]> GetAllClientesAsync()
+        {
+            IQueryable<Cliente> query = _dataContext.Clientes
+                .Include(c => c.Endereco);
+
+            query = query.AsNoTracking().OrderBy(c => c.Nome);
+
+            return await query.ToArrayAsync();
+        }
+
+        public async Task<Cliente[]> GetClientesByNomeAsync(string nome)
+        {
+            IQueryable<Cliente> query = _dataContext.Clientes
+                .Include(c => c.Endereco);
+
+            query = query.AsNoTracking().OrderBy(c => c.Nome)
+                        .Where(c => c.Nome.ToLower().Contains(nome.ToLower()));
+
+            return await query.ToArrayAsync();
+        }
+
+        public async Task<Cliente> GetClienteByIdAsync(Guid clienteId)
+        {
+            IQueryable<Cliente> query = _dataContext.Clientes
+                .Include(c => c.Endereco);
+
+            query = query.AsNoTracking()
+                        .Where(c => c.Id == clienteId);
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<Cliente> GetClienteByDocumentoAsync(string documento)
+        {
+            IQueryable<Cliente> query = _dataContext.Clientes;
+
+            query = query.AsNoTracking()
+                        .Where(c => c.Documento.Numero == documento);
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<Cliente> GetClienteByEmailAsync(string email)
+        {
+            IQueryable<Cliente> query = _dataContext.Clientes;
+
+            query = query.AsNoTracking()
+                        .Where(c => c.Email.Endereco == email);
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<bool> ExisteClienteComDocumentoAsync(string documento)
+        {
+            return await _dataContext.Clientes
+                .AnyAsync(c => c.Documento.Numero == documento);
+        }
+
+        public async Task<bool> ExisteClienteComEmailAsync(string email)
+        {
+            return await _dataContext.Clientes
+                .AnyAsync(c => c.Email.Endereco == email);
+        }
     }
 }
